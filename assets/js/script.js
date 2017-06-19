e6b4b96b08fa4f484914847dce98b2bba07901d1
Michael Davis <mhdavis1993@gmail.com>
2017-06-19 19:12:02 -0400

    added custom css styling and bootstrap page layout; removed img divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
   renderButtons();
 
-  $("#submit-button").on("click", function(event) {
+  $(document).on("click", "#submit-button", function(event) {
     // prevents default form "submit" behavior
     event.preventDefault();
     let $input = $("#search-input").val();
@@ -39,10 +39,10 @@ $(document).ready(function() {
     $input = "";
   });
 
-  $(document).on("click", ".query-button"
+  $(document).on("click", ".query-button",
     function(event) {
       event.preventDefault();
-      let queryItem = $(this).val();
+      let queryItem = $(this).text();
       let queryKey = "&api_key=dc6zaTOxFJmzC&limit=10";
       let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + queryItem + queryKey;
       $.ajax({
@@ -54,8 +54,6 @@ $(document).ready(function() {
         let products = response.data;
 
         for (let i = 0; i < products.length; i++) {
-          let gifDiv = $("<div>");
-          gifDiv.addClass("gif-layout");
 
           let gifImg = $("<img>");
           gifImg.addClass("gif")
@@ -64,20 +62,21 @@ $(document).ready(function() {
             .attr("data-still", products[i].images.fixed_height_still.url)
             .attr("data-animate", products[i].images.fixed_height.url);
 
-          gifDiv.append(gifImg);
-          $("#results").append(gifDiv);
+          $("#results").append(gifImg);
         }
       });
     });
 
-  $(".gif").on("click", function() {
+  $(document).on("click", ".gif", function() {
     let state = $(this).attr("data-state");
     if (state === "still") {
       $(this).attr("src", $(this).attr("data-animate"));
       $(this).attr("data-state", "animate");
+      $(this).css("border-color", "white");
     } else {
       $(this).attr("src", $(this).attr("data-still"));
       $(this).attr("data-state", "still");
+      $(this).css("border-color", "yellow");
     }
   });
 });
